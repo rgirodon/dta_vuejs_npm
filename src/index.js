1af4ce7ad98cb10e46a87acb913952fd04f9728b
rgirodon <remy.girodon@gmail.com>
2020-02-29 10:33:58 +0100

    Step 1 - advanced interpolation

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import Vue from 'vue';
 let vueInstance = new Vue({
     el: '#app',
     data: {
-        message: 'Hello from DTA !'
+        message: 'Hello from DTA !',
+        rawHtmlMessage: '<small>(Stay tuned)</small>',
+        textMessage: '2 < 3, isn\'t it ?',
+        imageSrc: 'img/logo.jpg'
     }
 });
