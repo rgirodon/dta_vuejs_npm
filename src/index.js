2bd780e80ac5a88983ffa6de49ef700575482f1a
rgirodon <remy.girodon@gmail.com>
2020-02-29 16:00:18 +0100

    Step 2 : instance lifecycle, computed properties, events handling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
 import Vue from 'vue';
+import _ from 'lodash';
 
 let vueInstance = new Vue({
     el: '#app',
     data: {
-        message: 'Hello from DTA !',
-        rawHtmlMessage: '<small>(Stay tuned)</small>',
+        message: 'Bonne journée la DTA !',
+        rawHtmlMessage: '<small>(Restez connectés)</small>',
         textMessage: '2 < 3, isn\'t it ?',
-        imageSrc: 'img/logo.jpg'
+        imageSrc: 'img/logo.jpg',
+        counter: 0
+    },
+    created: function() {
+        console.log('Vue instance created');
+    },
+    mounted: function() {
+        console.log('Vue instance mounted');
+    },
+    computed: {
+        reversedMessage: function() {
+            return this.message.split('').reverse().join('')
+        },
+        deburredMessage: function() {
+            return _.deburr(this.message);
+        }
+    },
+    methods: {
+        increment: function() {
+            this.counter++;
+        }
     }
 });
