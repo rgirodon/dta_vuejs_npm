5fdaaadce02b797ce5cedacd9cee5359cc4e26e8
rgirodon <remy.girodon@gmail.com>
2020-03-01 09:03:28 +0100

    Step 3 : affichage conditionnel et de liste

--- src/index.js
+++ src/index.js
@@ -8,7 +8,14 @@ let vueInstance = new Vue({
         rawHtmlMessage: '<small>(Restez connectés)</small>',
         textMessage: '2 < 3, isn\'t it ?',
         imageSrc: 'img/logo.jpg',
-        counter: 0
+        counter: 0,
+        randomNumber: 1,
+        items: [
+            {id: 1, label: 'Sols intérieur'},
+            {id: 2, label: 'Peinture'},
+            {id: 3, label: 'Plomberie'},
+            {id: 4, label: 'Sols extérieur'},
+        ]
     },
     created: function() {
         console.log('Vue instance created');
@@ -22,11 +29,22 @@ let vueInstance = new Vue({
         },
         deburredMessage: function() {
             return _.deburr(this.message);
+        },
+        noContentMessage: function() {
+            return 'No content displayed, because random number genetared was : ' + this.randomNumber;
         }
     },
     methods: {
         increment: function() {
             this.counter++;
+        },
+        displayContent: function() {
+
+            this.randomNumber = _.random(1, 10);
+
+            console.log(this.randomNumber);
+
+            return this.randomNumber % 2;
         }
     }
 });
