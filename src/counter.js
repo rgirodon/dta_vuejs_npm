f3a0b22ce6ee7ac1ecc83d9472af0ce044b661c6
rgirodon <remy.girodon@gmail.com>
2020-03-02 09:46:24 +0100

    Step 6 : router

--- src/counter.js
+++ src/counter.js
@@ -1,12 +1,24 @@
 export default {
     props: { 
-        count : Number, 
-        increment : Number
+        count : {
+            type : Number,
+            default: 0
+        }, 
+        increment : {
+            type : Number,
+            default: 1
+        }
     },
     methods: {
         incrementCount: function() {
             this.count = this.count + this.increment;
         }
     },
-    template: '<button v-on:click="incrementCount">You clicked me {{ count }} times.</button>'
+    template: '<button v-on:click="incrementCount">You clicked me {{ count }} times.</button>',
+    created: function() {
+        console.log('Counter instance created');
+    },
+    mounted: function() {
+        console.log('Counter instance mounted');
+    },
 }
