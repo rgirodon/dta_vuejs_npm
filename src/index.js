b34251f1afda9a60a2eb80d6b0246f2fae6ebeb2
rgirodon <remy.girodon@gmail.com>
2020-10-06 12:03:01 +0200

    Implement computed property and method for handling event

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import Vue from 'vue';
 let vueInstance = new Vue({
     el: '#app',
     data: {
-        message: 'Hello from TSE !'
+        message: 'Hello from TSE !',
+        counter: 0
+    },
+    computed: {
+        reversedMessage: function() {
+            return this.message.split('').reverse().join('')
+        }
+    },
+    methods: {
+        increment: function() {
+            this.counter++;
+        }
     }
 });
